286729c0570748bb29bc12d6dd47f6c9a881e35f
PedroZxK <pedrogabrielxx268@gmail.com>
2024-06-10 02:43:35 -0300

    .

--- atv9/assets/js/ex9.js
+++ atv9/assets/js/ex9.js
@@ -1,5 +1,3 @@
-/* Pedro Gabriel Moreira dos Santos nº26 */
-
 function toggleMenu() {
     const menuContent = document.getElementById('menuContent');
     if (menuContent.classList.contains('show')) {
@@ -31,13 +29,10 @@ document.addEventListener('click', function (event) {
 const button = document.getElementById('animateButton');
 
 button.addEventListener('click', function () {
-    // Verifica se o botão está na cor original
     const isOriginalColor = button.style.backgroundColor !== 'rgb(131, 59, 255)';
 
-    // Removendo a classe 'button-grow' para reiniciar a animação
     button.classList.remove('button-grow');
 
-    // Alternando a cor do botão entre a cor original e a cor especificada
     if (isOriginalColor) {
         button.style.backgroundColor = '#833bff';
         button.style.color = 'rgb(255, 240, 240)';
@@ -46,13 +41,10 @@ button.addEventListener('click', function () {
         button.style.color = '#fff';
     }
 
-    // Forçando o navegador a re-renderizar o botão antes de adicionar a classe novamente
     void button.offsetWidth;
 
-    // Adicionando a classe 'button-grow' para iniciar a animação
     button.classList.add('button-grow');
 
-    // Removendo a cor adicionada pelo JavaScript após um breve atraso
     setTimeout(function () {
         button.style.backgroundColor = '';
         button.style.color = '';
